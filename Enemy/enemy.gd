fca225fe17226933caa7463c197debe32ff35d48
b-van-b <115096508+b-van-b@users.noreply.github.com>
2023-06-18 23:56:57 +0800

    Replace player/enemy Sprite2D with AnimatedSprite2D

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export var movement_speed : float = 20.0
 
 @onready var player : CharacterBody2D = get_tree().get_first_node_in_group("player")
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	sprite.play("walk")
 
 func _physics_process(_delta):
 	var direction : Vector2 = global_position.direction_to(player.global_position)
--- Player/player.gd
+++ Player/player.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
 var movement_speed : float = 40.0
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(_delta):
 	movement()
 
 func movement() -> void:
-	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
-	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
-	var mov = Vector2(x_mov, y_mov)
+	var x_mov : float = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var y_mov : float = Input.get_action_strength("down") - Input.get_action_strength("up")
+	var mov : Vector2 = Vector2(x_mov, y_mov)
+	if mov.x > 0:
+		sprite.flip_h = true
+	elif mov.x < 0:
+		sprite.flip_h = false
+	
+	if mov.length() > 0:
+		sprite.animation = "walk"
+		sprite.play()
+	else:
+		sprite.animation = "stand"
+		sprite.stop()
+	
 	velocity = mov.normalized()*movement_speed
 	move_and_slide()
 
